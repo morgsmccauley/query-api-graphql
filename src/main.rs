152588a22daf067e1ab8c54693c09f50463eab38
Morgan McCauley <morgan@mccauley.co.nz>
2023-01-19 16:27:47 +1300

    Add postgres integration

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ use actix_web::{
     App, HttpResponse, HttpServer, Responder,
 };
 use actix_web_lab::respond::Html;
+use dotenv::dotenv;
 use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 mod schema;
 
-use crate::schema::{create_schema, Schema};
+use crate::schema::{create_schema, Context, Schema};
 
 /// GraphiQL playground UI
 #[get("/graphiql")]
@@ -25,17 +27,33 @@ async fn graphql_playground() -> impl Responder {
 
 /// GraphQL endpoint
 #[route("/graphql", method = "GET", method = "POST")]
-async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
-    let user = data.execute(&st, &()).await;
+async fn graphql(
+    schema: web::Data<Schema>,
+    db: web::Data<Pool<Postgres>>,
+    data: web::Json<GraphQLRequest>,
+) -> impl Responder {
+    let context = Context {
+        db: db.as_ref().to_owned(),
+    };
+    let user = data.execute(&schema, &context).await;
     HttpResponse::Ok().json(user)
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    dotenv().ok();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     // Create Juniper schema
     let schema = Arc::new(create_schema());
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Error building a connection pool");
 
     log::info!("starting HTTP server on port 8080");
     log::info!("GraphiQL playground: http://localhost:8080/graphiql");
@@ -44,6 +62,7 @@ async fn main() -> io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(Data::from(schema.clone()))
+            .app_data(Data::new(pool.clone()))
             .service(graphql)
             .service(graphql_playground)
             // the graphiql UI requires CORS to be enabled
--- src/schema.rs
+++ src/schema.rs
@@ -1,21 +1,20 @@
-use juniper::FieldResult;
 use juniper::{EmptySubscription, RootNode};
+use juniper::{FieldError, FieldResult};
 
-#[derive(GraphQLEnum)]
-enum Episode {
-    NewHope,
-    Empire,
-    Jedi,
+use juniper::{GraphQLInputObject, GraphQLObject};
+use sqlx::{FromRow, Pool, Postgres};
+
+pub struct Context {
+    pub db: Pool<Postgres>,
 }
 
-use juniper::{GraphQLEnum, GraphQLInputObject, GraphQLObject};
+impl juniper::Context for Context {}
 
-#[derive(GraphQLObject)]
+#[derive(GraphQLObject, FromRow)]
 #[graphql(description = "A humanoid creature in the Star Wars universe")]
 struct Human {
-    id: String,
+    id: i32,
     name: String,
-    appears_in: Vec<Episode>,
     home_planet: String,
 }
 
@@ -23,39 +22,46 @@ struct Human {
 #[graphql(description = "A humanoid creature in the Star Wars universe")]
 struct NewHuman {
     name: String,
-    appears_in: Vec<Episode>,
     home_planet: String,
 }
 
 pub struct QueryRoot;
 
-#[juniper::graphql_object]
+#[juniper::graphql_object(context = Context)]
 impl QueryRoot {
-    fn human(_id: String) -> FieldResult<Human> {
-        Ok(Human {
-            id: "1234".to_owned(),
-            name: "Luke".to_owned(),
-            appears_in: vec![Episode::NewHope],
-            home_planet: "Mars".to_owned(),
-        })
+    async fn humans(context: &Context) -> FieldResult<Vec<Human>> {
+        sqlx::query_as::<_, Human>("SELECT * from humans")
+            .fetch_all(&context.db)
+            .await
+            .map_err(FieldError::from)
+    }
+
+    async fn human(context: &Context, id: i32) -> FieldResult<Human> {
+        sqlx::query_as::<_, Human>("SELECT * from humans where id = $1")
+            .bind(id)
+            .fetch_one(&context.db)
+            .await
+            .map_err(FieldError::from)
     }
 }
 
 pub struct MutationRoot;
 
-#[juniper::graphql_object]
+#[juniper::graphql_object(context = Context)]
 impl MutationRoot {
-    fn create_human(new_human: NewHuman) -> FieldResult<Human> {
-        Ok(Human {
-            id: "1234".to_owned(),
-            name: new_human.name,
-            appears_in: new_human.appears_in,
-            home_planet: new_human.home_planet,
-        })
+    async fn create_human(context: &Context, new_human: NewHuman) -> FieldResult<Human> {
+        sqlx::query_as::<_, Human>(
+            "INSERT INTO humans (name, home_planet) VALUES ($1, $2) RETURNING *",
+        )
+        .bind(new_human.name)
+        .bind(new_human.home_planet)
+        .fetch_one(&context.db)
+        .await
+        .map_err(FieldError::from)
     }
 }
 
-pub type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription>;
+pub type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription<Context>>;
 
 pub fn create_schema() -> Schema {
     Schema::new(QueryRoot {}, MutationRoot {}, EmptySubscription::new())
