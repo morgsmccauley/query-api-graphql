0ef59c8ec7d0987ce9202657b6f49c3be5c24198
Morgan McCauley <morgan@mccauley.co.nz>
2023-01-23 16:40:30 +1300

    Snapshot test schema SDL

--- src/schema.rs
+++ src/schema.rs
@@ -74,3 +74,8 @@ pub type Schema = RootNode<'static, Query, Mutation, EmptySubscription<Context>>
 pub fn create_schema() -> Schema {
     Schema::new(Query {}, Mutation {}, EmptySubscription::new())
 }
+
+#[test]
+fn schema_snapshot() {
+    insta::assert_snapshot!(create_schema().as_schema_language())
+}
