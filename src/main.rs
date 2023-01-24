308bb0a4a466171a84ef1bab0880f99e1303dda3
Morgan McCauley <morgan@mccauley.co.nz>
2023-01-25 09:57:26 +1300

    Migrate from `juniper` to `async-graphql`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, sync::Arc};
+use std::io;
 
 use actix_cors::Cors;
 use actix_web::{middleware, web::Data, App, HttpServer};
@@ -16,8 +16,6 @@ async fn main() -> io::Result<()> {
     dotenv().ok();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let schema = Arc::new(create_schema());
-
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let port = std::env::var("PORT")
         .expect("PORT must be set")
@@ -30,13 +28,14 @@ async fn main() -> io::Result<()> {
         .await
         .expect("Error building a connection pool");
 
+    let schema = create_schema().data(pool).finish();
+
     log::info!("starting HTTP server on port {}", port);
     log::info!("GraphiQL playground: http://localhost:{}/graphiql", port);
 
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::from(schema.clone()))
-            .app_data(Data::new(pool.clone()))
+            .app_data(Data::new(schema.clone()))
             .service(graphql)
             .service(graphql_playground)
             .wrap(Cors::permissive())
--- src/schema.rs
+++ src/schema.rs
@@ -1,19 +1,11 @@
-use juniper::{EmptySubscription, RootNode};
-use juniper::{FieldError, FieldResult};
-
 use sqlx::{FromRow, Pool, Postgres};
 
-pub struct Context {
-    pub db: Pool<Postgres>,
-}
-
-impl juniper::Context for Context {}
-
-#[derive(juniper::GraphQLScalarValue)]
-#[graphql(transparent)]
+#[derive(serde::Serialize, serde::Deserialize)]
 struct JsonString(String);
 
-#[derive(FromRow, juniper::GraphQLObject)]
+async_graphql::scalar!(JsonString);
+
+#[derive(FromRow, async_graphql::SimpleObject)]
 #[allow(dead_code)]
 struct IndexerStorage {
     function_name: String,
@@ -23,59 +15,66 @@ struct IndexerStorage {
 
 pub struct Query;
 
-#[juniper::graphql_object(context = Context)]
+#[async_graphql::Object]
 impl Query {
     async fn get(
-        context: &Context,
+        &self,
+        context: &async_graphql::Context<'_>,
         function_name: String,
         key: String,
-    ) -> FieldResult<Option<JsonString>> {
+    ) -> async_graphql::Result<Option<JsonString>> {
+        let pool = context.data::<Pool<Postgres>>()?;
+
         match sqlx::query_as::<_, IndexerStorage>(
             "SELECT * FROM indexer_storage WHERE function_name = $1 AND key_name = $2",
         )
         .bind(function_name)
         .bind(key)
-        .fetch_optional(&context.db)
+        .fetch_optional(pool)
         .await
         {
             Ok(Some(storage)) => Ok(Some(JsonString(storage.value))),
             Ok(None) => Ok(None),
-            Err(err) => Err(FieldError::from(err)),
+            Err(err) => Err(err.into()),
         }
     }
 }
 
 pub struct Mutation;
 
-#[juniper::graphql_object(context = Context)]
+#[async_graphql::Object]
 impl Mutation {
     async fn set(
-        context: &Context,
+        &self,
+        context: &async_graphql::Context<'_>,
         function_name: String,
         key: String,
         data: JsonString,
-    ) -> FieldResult<JsonString> {
+    ) -> async_graphql::Result<JsonString> {
+        let pool = context.data::<Pool<Postgres>>()?;
+
         match sqlx::query_as::<_, IndexerStorage>(
             "INSERT INTO indexer_storage (function_name, key_name, value) VALUES ($1, $2, $3) RETURNING *"
         )
             .bind(function_name)
             .bind(key)
             .bind(data.0)
-            .fetch_one(&context.db)
+            .fetch_one(pool)
             .await {
                 Ok(storage) => Ok(JsonString(storage.value)),
-                Err(err) => Err(FieldError::from(err))
+                Err(err) => Err(err.into())
             }
     }
 }
 
-pub type Schema = RootNode<'static, Query, Mutation, EmptySubscription<Context>>;
+pub type Schema = async_graphql::Schema<Query, Mutation, async_graphql::EmptySubscription>;
 
-pub fn create_schema() -> Schema {
-    Schema::new(Query {}, Mutation {}, EmptySubscription::new())
+pub fn create_schema(
+) -> async_graphql::SchemaBuilder<Query, Mutation, async_graphql::EmptySubscription> {
+    async_graphql::Schema::build(Query, Mutation, async_graphql::EmptySubscription)
 }
 
 #[test]
 fn schema_snapshot() {
-    insta::assert_snapshot!(create_schema().as_schema_language())
+    insta::assert_snapshot!(create_schema().finish().sdl())
 }
